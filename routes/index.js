c3c24d1fa7db1f3600e50bce10d221139381e22e
Chalindu Kodikara <Chalindu.18@cse.mrt.ac.lk>
2021-02-15 21:34:49 +0530

    Initialized to code

--- routes/index.js
+++ routes/index.js
@@ -5,21 +5,22 @@ const mysql = require("mysql");
 const db = mysql.createPool({
   host: "localhost",
   user: "root",
-  password: "password",
+  password: "0719773008",
   database: "electrica"
 });
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  const sqlInsert =
-    "INSERT INTO reply (Reply_ID, Feedback_ID, Reply) VALUES ('2', '5', 'Database is connected');";
-  db.query(sqlInsert, (err, result) => {
-    if(err) {
-      res.send(err);
-    } else {
-      res.send('Success');
-    }
-  });
+  // const sqlInsert =
+  //   "INSERT INTO reply (Reply_ID, Feedback_ID, Reply) VALUES ('2', '5', 'Database is connected');";
+  // db.query(sqlInsert, (err, result) => {
+  //   if(err) {
+  //     res.send(err);
+  //   } else {
+  //     res.send('Success');
+  //   }
+  // });
+  res.send('hi');
 });
 
 module.exports = router;
