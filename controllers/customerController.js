98b708799c10d6aa06d786cac573f6c06c2ff275
Chalindu Kodikara <Chalindu.18@cse.mrt.ac.lk>
2021-02-17 23:53:24 +0530

    ss

--- controllers/customerController.js
+++ controllers/customerController.js
@@ -1,21 +1,3 @@
-<<<<<<< HEAD
-const Customer = require('../models/customerModel');
-
-
-const getAllAddress = (req, res) => {
-    
-        // .then((employees) => {
-        //     res.statusCode = 200;
-        //     res.setHeader('Content-Type', 'application/json');
-        //     res.json(employees);
-        // }, (err) => next(err))
-        // .catch((err) => next(err));
-}
-
-const registerAction = (req, res) => {
-    
-}
-=======
 const { resourceUsage } = require('process');
 const Customer = require('../model/customerModel');
 
@@ -32,7 +14,6 @@ const getAddresses = (req,res,next)=>{
 }
 
 getAddresses();
->>>>>>> 478aece5a6d9013698833309701596f5ad5b3f54
 
 module.exports = {
     getAddresses
