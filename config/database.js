93355d2a440c4a9793ef47e66d550fa5717de008
Chalindu Kodikara <Chalindu.18@cse.mrt.ac.lk>
2021-02-17 16:15:57 +0530

    Fixed some bugs

--- config/database.js
+++ config/database.js
@@ -5,16 +5,23 @@ const database = mysql.createPool({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-});
+})
 
-database.connect((err) => {
-  if (err) {
-    return console.log(`Something went wrong while connecting to the database
-    Error : ${err}
-    `);
-  }
+// database.connect(function(err) {
+//   if (err) throw err;
+//   console.log("Connected!");
+// })
+// ;
 
-  console.log("Connected to the MySQL server");
-});
+// database.connect((err) => {
+//   if (err) {
+//     return console.log(`Something went wrong while connecting to the database
+//     Error : ${err}
+//     `);
+//   }
+
+//   console.log("Connected to the MySQL server");
+// });
+
 
 module.exports = database;
--- routes/index.js
+++ routes/index.js
@@ -1,26 +1,26 @@
 var express = require("express");
 var router = express.Router();
-const mysql = require("mysql");
+const db = require("../config/database");
 
-const db = mysql.createPool({
-  host: "localhost",
-  user: "root",
-  password: "password",
-  database: "electrica"
-});
+
+
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  // const sqlInsert =
-  //   "INSERT INTO reply (Reply_ID, Feedback_ID, Reply) VALUES ('2', '5', 'Database is connected');";
-  // db.query(sqlInsert, (err, result) => {
-  //   if(err) {
-  //     res.send(err);
-  //   } else {
-  //     res.send('Success');
-  //   }
-  // });
-  res.send('hi');
+  const sqlInsert =
+    "INSERT INTO cart (NumOfItems) VALUES ('10');";
+    db.connect(function(err) {
+      if (err) throw err;
+      console.log("Connected!");
+    });
+  db.query(sqlInsert, (err, result) => {
+    if(err) {
+      res.send(err);
+    } else {
+      res.send('Success');
+    }
+  });
+  // res.send('hi');
 });
 
 module.exports = router;
