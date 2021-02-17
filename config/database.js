1a2c6192042088a1bdf065c98bfeadee77c27503
chalindu <38396435+chalindu@users.noreply.github.com>
2021-02-17 23:17:50 +0530

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,19 +1,18 @@
 const mysql = require("mysql");
 
-const database = mysql.createPool({
+const database = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
 })
 
-database.getConnection(function(err) {
+database.connect(function(err) {
   if (err) {
     return console.log(`Something went wrong while connecting to the database
         Error : ${err}
         `);
   }
-
   console.log("Connected to the MySQL server");
 });
 
